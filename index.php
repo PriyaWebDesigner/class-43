b36774642eb2923064e70e308840a74ea36adb44
PriyaWebDesigner <naskartanu@111gmail.com>
2024-07-07 10:33:53 +0530

    Access Modifier

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+// 1. Use of this-> keyword
 class Car{
 
     public $name;
@@ -10,13 +10,73 @@ class Car{
         $this->engine_cc = $engineParam;
     }
 
-    public function democar(){
+    public function demoCar(){
         echo 'The Car name is ='.$this->name.'<br>';
         echo 'The Engine name is ='.$this->engine_cc.'<br>';
     }
+
+    // public function demoCar2(){
+    //     $this ->demoCar();
+    // }
 }
 
+
+
 $car= new Car('BMW', '4000cc');
-$car->democar();
+$car->demoCar();
+// $car->demoCar2();
+
+?>
+
+<?php
+
+class Car2{
+
+    protected $name;
+    protected $engine_cc;
+
+    protected function demoCar2(){
+        echo 'The Car Name is ='.$this->name.'<br>';
+        echo 'The Engine Name is ='.$this->engine_cc.'<br>';
+    }
+
+    private function demoCar3(){
+        echo 'This is a Private Car';
+    }
+
+    public function demoCar4(){
+        $this->demoCar2();
+    }
+
+    protected function demoCar5(){
+        $this->demoCar3();
+    }
+}
+
+class car3 extends Car2{
+    public function show(){
+       echo 'Demo <br>';
+       $this->demoCar5();
+       echo '<br>';
+       $this->demoCar2();
+
+       $this->name = 'BMW';
+       $this->engine_cc = '4000cc';
+    
+    }
+
+}
+
+// $car= new Car2();
+// $car->name = 'BMW';
+// $car->engine_cc = '4000cc';
+// $car->demoCar2();
+
+$carObject = new Car3();
+// $carObject->name = 'BMW';
+// $carObject->engine_cc = '4000cc';
+$carObject->show();
+$carObject->demoCar4();
+
 
 ?>
